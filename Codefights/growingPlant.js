fa3cd2d1582531805b3f0eaef486be38c051df8a
Airas Yaqub <airasyaqub6@gmail.com>
2018-02-25 21:54:17 +0300

    new algo added

---
+++ Codefights/growingPlant.js
@@ -0,0 +1,46 @@
+/*Each day a plant is growing by upSpeed meters. 
+Each night that plant's height decreases by downSpeed meters due to the lack of sun heat. 
+Initially, plant is 0 meters tall. We plant the seed at the beginning of a day. 
+We want to know when the height of the plant will reach a certain level.
+Example
+For upSpeed = 100, downSpeed = 10 and desiredHeight = 910, the output should be
+growingPlant(upSpeed, downSpeed, desiredHeight) = 10.*/
+
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+        var currentHeight=0;
+   for(var i=1;;i++){
+           currentHeight+=upSpeed;
+           if(currentHeight>=desiredHeight){
+                   break;
+           }
+           else{
+                   currentHeight-=downSpeed;
+           }
+   }
+        return i;
+}
+ 
+
+
+
+
+
+
+/*function growingPlant(upSpeed, downSpeed, desiredHeight) {
+        var minus=upSpeed-downSpeed;
+        var i=0;
+        var res=0;
+        while(res<desiredHeight){
+                i++;
+                res=minus*i;
+                console.log(minus,res,i);
+                
+        }
+        console.log('yo'+res,minus,i,i-1);
+        if(res==desiredHeight){
+                return i; 
+        }
+        else{
+                return i-1;
+        }
+                }*/
